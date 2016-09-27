71a81c9d461e642bddfe1f50e51859b4a1655214
ajb904 <a.j.baylay@soton.ac.uk>
2016-09-27 10:56:32 +0100

    More work on contig clustering

--- scripts/contig_clustering.py
+++ scripts/contig_clustering.py
@@ -6,13 +6,16 @@ import os
 # Cluster contigs from an assembly using CD-HIT-EST at a variety of different identity levels. Shorter contigs
 # should match longer contigs over 100% of their length
 
-CD_HIT_EST = 'cd-hit-est'
+CD_HIT_EST = '~/Downloads/cd-hit-v4.6.6-2016-0711/cd-hit-est'
 
 def run_CDHIT(fasta, identity, outdir, cov=1.0):
     # identity should be given as a decimal between 0 and 1, up to 2 dp
     percent_id = float(identity)*100
     outfile_suffix = ('%.0f' % percent_id).replace('.','_') + '.fasta'
     outfile = fasta.replace('.fasta', outfile_suffix)
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
     outfile = os.path.join(outdir, outfile)
 
     cdhit_cline = [CD_HIT_EST,
@@ -24,7 +27,10 @@ def run_CDHIT(fasta, identity, outdir, cov=1.0):
                    '-T', '0',
                    '-M', '32000']
 
-    proc = subprocess.Popen(cdhit_cline, stdout=subprocess.PIPE)
+    cdhit_cline = ' '.join(cdhit_cline)
+    print cdhit_cline
+
+    proc = subprocess.Popen(cdhit_cline, shell=True, stdout=subprocess.PIPE)
 
     stdout = proc.communicate()[0]
 
@@ -35,10 +41,10 @@ def run_CDHIT(fasta, identity, outdir, cov=1.0):
     return outfile, num_clusters
 
 if __name__=='__main__':
-    percents = range(50, 101, 5)
+    percents = range(80, 101, 5)
 
     for p in percents:
         identity = float(p)/100
-        s = run_CDHIT('test.fasta', identity, 'test')
+        s = run_CDHIT('temp.fasta', identity, 'test')
         print s
 
