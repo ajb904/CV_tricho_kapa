95c76a2f492f48c62947f8ac6bdca7cf60e5a9ba
ajb904 <a.j.baylay@soton.ac.uk>
2016-09-27 11:30:31 +0100

    Added cluster counting function

--- scripts/contig_clustering.py
+++ scripts/contig_clustering.py
@@ -2,51 +2,69 @@
 
 import subprocess
 import os
+from Bio import SeqIO
+
 
 # Cluster contigs from an assembly using CD-HIT-EST at a variety of different identity levels. Shorter contigs
 # should match longer contigs over 100% of their length
 
 CD_HIT_EST = '~/Downloads/cd-hit-v4.6.6-2016-0711/cd-hit-est'
 
-def run_CDHIT(fasta, identity, outdir, cov=1.0):
+def run_CDHIT(fasta, identity, word_size, outdir, cov=1.0):
     # identity should be given as a decimal between 0 and 1, up to 2 dp
     percent_id = float(identity)*100
-    outfile_suffix = ('%.0f' % percent_id).replace('.','_') + '.fasta'
-    outfile = fasta.replace('.fasta', outfile_suffix)
 
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
-    outfile = os.path.join(outdir, outfile)
+    outfile = get_outfile(fasta, percent_id, outdir)
 
     cdhit_cline = [CD_HIT_EST,
                    '-i', fasta,
                    '-o', outfile,
                    '-c', '%.1f' % float(identity),
+                   '-n', str(word_size),
                    '-aS', '1.0',
                    '-d', '0',
                    '-T', '0',
-                   '-M', '32000']
+                   '-M', '32000',
+                   '>', '%s.log' % outfile]
 
     cdhit_cline = ' '.join(cdhit_cline)
-    print cdhit_cline
 
-    proc = subprocess.Popen(cdhit_cline, shell=True, stdout=subprocess.PIPE)
+    subprocess.call(cdhit_cline, shell=True)
 
-    stdout = proc.communicate()[0]
+    return outfile
 
-    num_clusters=0
-    for line in stdout:
-        print line
-        if line.endswith('clusters'):
-            num_clusters = line.split[3]
+def get_num_clusters(clustered_file, size_threshold):
+    #Return number of clusters in a file that are greater than a specified size)
 
-    return outfile, num_clusters
+    fasta = SeqIO.parse(clustered_file, 'fasta')
+
+    seq_count = 0
+    for seq in fasta:
+        if len(seq.seq) > int(size_threshold):
+            seq_count += 1
+
+    return seq_count
+
+def get_outfile(fasta, percent_id, outdir):
+    outfile = fasta.replace('.fasta', '%d.fasta' % percent_id)
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    outfile = os.path.join(outdir, outfile)
+
+    return outfile
 
 if __name__=='__main__':
-    percents = range(80, 101, 5)
+    percents = range(75, 101, 5)
+    word_sizes = range(4, 11)
 
-    for p in percents:
+    input_fasta = 'temp.fasta'
+    outdir = 'test'
+
+    for p, ws in zip(percents, word_sizes):
         identity = float(p)/100
-        s = run_CDHIT('temp.fasta', identity, 'test')
-        print s
 
+        clustered_file = run_CDHIT(input_fasta, identity, ws, outdir)
+        print clustered_file
+
+        print get_num_clusters(clustered_file, 1000)
